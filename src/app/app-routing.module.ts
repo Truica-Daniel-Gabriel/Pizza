fc93d8e989ccaa4b4e54905fa88db317d395d60e
Dani <truicadaniel@yahoo.com>
2023-09-19 16:26:02 +0300

    Folder hierarchy created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => 
+      import('./pages/customer/customer.module').then((m) => m.CustomerModule),
+  },
+
+  {
+    path:'auth',
+    loadChildren: () => 
+      import('./pages/auth/auth.module').then((m)=>m.AuthModule)
+  },
+
+  {
+    path:'admin',
+    loadChildren: () =>
+      import('./pages/admin/admin.module').then((m)=>m.AdminModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
